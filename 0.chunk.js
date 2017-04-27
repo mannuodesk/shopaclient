eae56fd4bc2e936f2f1ff076b61105162b27fa30
umer shahzad <umer shahzad>
2017-04-27 18:22:14 +0500

    toDeploy

--- 0.chunk.js
+++ 0.chunk.js
@@ -14413,7 +14413,7 @@ exports.SigninSignupService = SigninSignupService;
 "use strict";
 "use strict";
 var UrlService = (function () {
-    // public baseUrl:string = "http://localhost:13377/";
+    //public baseUrl:string = "http://localhost:13377/";
     function UrlService() {
         this.baseUrl = "http://shopabackend.azurewebsites.net/";
     }
